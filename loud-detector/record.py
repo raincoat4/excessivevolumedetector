0bb7cc66bd350f3c22ab2ad2e84de585a3339aa1
raincoat4 <lr1tmrr@gmail.com>
2024-01-24 15:05:07 -0800

    changed some variable names to be more consistent

--- loud-detector/record.py
+++ loud-detector/record.py
@@ -12,28 +12,26 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
 
 recording = False
-recorded_data = []
+recordedData = []
 #if you take this from me change this to where you want to save the recordings
-save_directory = "/Users/liamrogers/Documents/recordings"
+saveDirectory = "/Users/liamrogers/Documents/recordings"
 
-def save_recording():
-    global recorded_data
-    global start_time
+def saveRecording(recordedData, startTime):
 
-    end_time = time.time()
-    print("Recording ended at", time.ctime(end_time))
+    endTime = time.time()
+    print("Recording ended at", time.ctime(endTime))
     #convert time_t object to readable time
-    datetimeObj = datetime.fromtimestamp(start_time)
+    datetimeObj = datetime.fromtimestamp(startTime)
     readableTime = datetimeObj.strftime("%Y-%m-%d %H-%M-%S")
-    file_name = os.path.join(save_directory, f"{readableTime}.wav")
-    wf = wave.open(file_name, 'wb')
+    fileName = os.path.join(saveDirectory, f"{readableTime}.wav")
+    wf = wave.open(fileName, 'wb')
     wf.setnchannels(1)
     wf.setsampwidth(pyaudio.PyAudio().get_sample_size(pyaudio.paInt16))
     wf.setframerate(44100)
-    wf.writeframes(b''.join(struct.pack('h', samp) for samp in recorded_data))
+    wf.writeframes(b''.join(struct.pack('h', samp) for samp in recordedData))
     wf.close()
 
-    print(f"Recording saved as {file_name}")
+    print(f"Recording saved as {fileName}")
 
 try:
     while True:
@@ -41,22 +39,23 @@ try:
         data = stream.read(1024)
         samples = struct.unpack('h' * 1024, data)
 
-        #rms is apparently better for this i dont really know why
+        #rms is apparently better than taking the max sample for this
+        #also max sample didnt even work properly but this does
         rms = max(0, int((sum([(samp ** 2) for samp in samples]) / len(samples)) ** 0.5))
 
         if rms > threshold and not recording:
             recording = True
-            start_time = time.time()
-            print("Recording started at", time.ctime(start_time))
-            recorded_data = []  #start a new buffer
+            startTime = time.time()
+            print("Recording started at", time.ctime(startTime))
+            recordedData = []  #start a new buffer
 
         if recording:
-            recorded_data.extend(samples)
+            recordedData.extend(samples)
 
             #once the audio goes below threshold again, end recording
             if rms <= threshold:
                 recording = False
-                save_recording()
+                saveRecording(recordedData, startTime)
 
         #print messages as it goes over
         if recording:
@@ -71,4 +70,4 @@ finally:
     p.terminate()
 
     if recording:
-        save_recording()
+        saveRecording(recordedData, startTime)
